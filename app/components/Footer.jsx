5df2ff90f7e545f28092c47f0d42b7613478bcba
Abdullah-webd <ajalabdllh@gmail.com>
2025-04-02 16:39:03 +0100

    fourth commit

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = ({isDarkMode}) => {
 
             <div className='w-max flex items-center gap-2 mx-auto'>
                 <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6'/>
-                webmastersmma@gmail.com
+               <a href="mailto:webmastersmma@gmail.com">webmastersmma@gmail.com</a> 
             </div>
         </div>
 
